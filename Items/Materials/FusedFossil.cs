c9dc15c7d09aa0ca5da9f32c6390f6c9fd8e1f5a
post <>
2018-03-15 20:43:07 -0400

    add quickmaterial and fusedfossil

---
+++ Items/Materials/FusedFossil.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace LacunaMod.Items.Materials
+{
+    public class FusedFossil : QuickMaterial
+    {
+        public override void Data()
+        {
+            name = "Fused Fossil";
+            rarity = 2;
+            value = 8;
+        }
+    }
+}
---
+++ Items/Materials/QuickMaterial.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LacunaMod.Items.Materials
+{
+    public abstract class QuickMaterial : ModItem
+    {
+        public string name;
+        public string tooltip;
+        public int[] size = new int[] { 32, 32 };
+        public int rarity = 0;
+        public int value = 0;
+
+        public abstract void Data();
+
+        public override void SetStaticDefaults()
+        {
+            Data();
+            DisplayName.SetDefault(name);
+            Tooltip.SetDefault(tooltip);
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = size[0];
+            item.height = size[1];
+            item.rare = rarity;
+            item.value = value;
+        }
+    }
+}
