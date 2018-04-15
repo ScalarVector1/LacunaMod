5cadfe71cf5f2d0a128e3ea6bc186dab4ab007f8
Fro Zen <38363715+Frozenreflex@users.noreply.github.com>
2018-04-15 16:11:37 +0000

    fuseglass shards

---
+++ Projectiles/GlassShard.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LacunaMod.Projectiles
+{
+    public class GlassShard : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Glass Shard");
+			
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.SpikyBall);
+		    projectile.width = 10;
+            projectile.height = 12;
+        }
+    }
+}
