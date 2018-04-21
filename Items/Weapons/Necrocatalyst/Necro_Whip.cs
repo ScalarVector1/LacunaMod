caa72e26d40ed233f114da0235b7915336aa0fd5
NotAScalie <bobfishkins23@yahoo.com>
2018-04-21 13:04:21 -0400

    fix

--- Items/Weapons/Necrocatalyst/Necro_Whip.cs
+++ Items/Weapons/Necrocatalyst/Necro_Whip.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace LTEST.Items
+namespace LacunaMod.Items.Weapons.Necrocatalyst
 {
 	public class Necro_Whip : ModItem
     { 
