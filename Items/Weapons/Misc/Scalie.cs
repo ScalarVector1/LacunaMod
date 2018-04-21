5f71f4f69fa3429c1e299f72ea3a034d72507ef7
NotAScalie <bobfishkins23@yahoo.com>
2018-04-21 18:42:40 -0400

    Nothing

---
+++ Items/Weapons/Misc/Scalie.cs
@@ -0,0 +1,57 @@
+﻿using LacunaMod.Projectiles.Misc;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace LacunaMod.Items.Weapons.Misc
+{
+	public class Scalie : ModItem
+    { 
+
+                public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("Scalie's Lightning");
+        Tooltip.SetDefault("No curses attached");
+    }
+
+
+        public override void SetDefaults()
+        {
+            item.shootSpeed = 14f;
+            item.damage = 800;
+            item.knockBack = 0.5f;
+            item.useStyle = 5;
+            item.useAnimation = 21;
+            item.useTime = 1;
+            item.width = 40;
+            item.height = 40;
+            item.maxStack = 1;
+            item.rare = 13;
+
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.autoReuse = true;
+            item.melee = true;
+
+            item.UseSound = SoundID.Item72;
+            item.shoot = mod.ProjectileType<Greem>();
+        }
+
+             public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 4 + Main.rand.Next(0); // 4 or 5 shots
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(2)); // 30 degree spread.
+                                                                                                                 // If you want to randomize the speed to stagger the projectiles
+                                                                                                                 // float scale = 1f - (Main.rand.NextFloat() * .3f);
+                                                                                                                 // perturbedSpeed = perturbedSpeed * scale; 
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return false; // return false because we don't want tmodloader to shoot projectile
+        }
+
+    }
+    
+}
---
+++ Projectiles/Misc/Greem.cs
@@ -0,0 +1,55 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LacunaMod.Projectiles.Misc
+{
+    public class Greem : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.penetrate = 1;
+            projectile.aiStyle = 3;
+            projectile.velocity.X = 1f;
+            projectile.velocity.Y = 1f;
+            projectile.timeLeft = 55;
+        }
+
+        public override void AI()
+        {
+            if (Main.rand.Next(2) == 0)
+            {
+                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, mod.DustType("Greem_Dust"), projectile.velocity.X * 0.9f, projectile.velocity.Y * 0.9f);
+            }
+            int willdie = 1;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+
+        {
+           
+            projectile.timeLeft = 0;
+            if (projectile.timeLeft <= 0)
+            {
+                projectile.Kill();
+            }
+            else
+            {
+                if (projectile.velocity.X != oldVelocity.X)
+                {
+                    projectile.velocity.X = -oldVelocity.X;
+                }
+                if (projectile.velocity.Y != oldVelocity.Y)
+                {
+                    projectile.velocity.Y = -oldVelocity.Y;
+                }
+                Main.PlaySound(SoundID.Item10, projectile.position);
+            }
+            return false;
+        }
+    }
+}
