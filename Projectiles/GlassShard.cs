dbc306a525247505eb896a89db32c5169d7f1607
Fro Zen <frozenreflex1@gmail.com>
2018-04-22 00:32:37 +0000

    glass shards arent working properly so i removed the code that wasnt needed

--- Projectiles/GlassShard.cs
+++ Projectiles/GlassShard.cs
@@ -17,21 +17,5 @@ namespace LacunaMod.Projectiles
             projectile.width = 10;
             projectile.height = 12;
         }
-        bool kill = false;
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
-        {
-            int hits = Main.rand.Next(2, 3);
-            if (hits == 0)
-            {
-                kill = true;
-            }
-        }
-        public override void Kill(int timeLeft)
-        {
-            if (kill)
-            {
-                timeLeft = 0;
-            }
-        }
     }
 }
