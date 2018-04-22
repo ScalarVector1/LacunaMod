a49a41a60edec5841d6154aaa5f466542ab19c4a
NotAScalie <bobfishkins23@yahoo.com>
2018-04-22 16:53:04 -0400

    added stuff WIP WARNING WILL NOT BUILD

---
+++ Items/Weapons/Necrocatalyst/Necro_Staff.cs
@@ -0,0 +1,57 @@
+﻿using LacunaMod.Projectiles.Necro_Items;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace LacunaMod.Items.Weapons.Necrocatalyst
+{
+	public class Necro_Staff : ModItem
+    { 
+
+                public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("The Blue Torch");
+        Tooltip.SetDefault("Lighting the way to darkness");
+    }
+
+	
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 8f;
+			item.damage = 14;
+			item.knockBack = 3f;
+			item.useStyle = 1;
+			item.useAnimation = 35;
+			item.useTime = 35;
+			item.width = 40;
+			item.height = 40;
+			item.maxStack = 1;
+			item.rare = 3;
+            item.mana = 35;
+
+			item.noUseGraphic = false;
+			item.noMelee = true;
+			item.autoReuse = false;
+            item.magic = true;
+
+            item.UseSound = SoundID.Item20;
+			item.shoot = mod.ProjectileType<Necro_Staff_Projectile>();
+
+       
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 5 + Main.rand.Next(0); // 4 or 5 shots
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(5)); // 30 degree spread.
+                                                                                                                // If you want to randomize the speed to stagger the projectiles
+                                                                                                                 float scale = 1f - (Main.rand.NextFloat() * .8f);
+                                                                                                                 perturbedSpeed = perturbedSpeed * scale; 
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return false; // return false because we don't want tmodloader to shoot projectile
+        }
+    }
+}
---
+++ Projectiles/Necro_Items/Necro_Staff_Projectile.cs
@@ -0,0 +1,73 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LacunaMod.Projectiles.Necro_Items
+{
+    public class Necro_Staff_Projectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.friendly = true;
+            projectile.magic = true;
+            projectile.penetrate = 4;
+            projectile.timeLeft = 200;
+        }
+
+        public override void AI()
+        {
+            if (Main.rand.Next(2) == 0)
+            {
+                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, mod.DustType("Necro_Dust"), projectile.velocity.X * 0.9f, projectile.velocity.Y * 0.9f);
+            }
+            int willdie = 1;
+
+
+
+            Main.PlaySound(SoundID.Item10, projectile.position);
+            int num4;
+            for (int r; r < 20; r++)
+            {
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, mod.DustType("Necro_Dust"), 0.2f, 0.2f);
+            }
+
+
+            bool flag10 = false;
+            if (velocity.X != projectile.velocity.X)
+            {
+                if (Math.Abs(velocity.X) > 4f)
+                {
+                    flag10 = true;
+                }
+                projectile.position.X = projectile.position.X + projectile.velocity.X;
+                projectile.velocity.X = (0f - velocity.X) * 0.2f;
+            }
+            if (velocity.Y != projectile.velocity.Y)
+            {
+                if (Math.Abs(velocity.Y) > 4f)
+                {
+                    flag10 = true;
+                }
+                projectile.position.Y = projectile.position.Y + projectile.velocity.Y;
+                projectile.velocity.Y = (0f - velocity.Y) * 0.2f;
+            }
+            ai[0] = 1f;
+            if (flag10)
+            {
+                netUpdate = true;
+                Collision.HitTiles(projectile.position, projectile.velocity, projectile.width, projectile.height);
+                Main.PlaySound(0, (int)projectile.position.X, (int)projectile.position.Y, 1, 1f, 0f);
+            }
+            if (projectile.wet)
+            {
+                wetVelocity = projectile.velocity;
+            }
+        }
+    }
+
+    
+    
+}
