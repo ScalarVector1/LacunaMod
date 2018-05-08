9a58907d82d10728d3c95b6581e4ead19dbcaf88
Fro Zen <frozenreflex1@gmail.com>
2018-05-08 20:30:46 +0000

    a

--- LCPlr.cs
+++ LCPlr.cs
@@ -147,7 +147,19 @@ namespace LacunaMod
 
             }
 
+            if (teletimer == 1)
+            {
+                int CloakDirection = player.direction;
+                for (int r = 0; r > 15; r++)
+                {
+                    Dust dust;
+                    // You need to set position depending on what you are doing. You may need to subtract width/2 and height/2 as well to center the spawn rectangle.
+                    Vector2 position = Main.LocalPlayer.Center;
+                    dust = Main.dust[Terraria.Dust.NewDust(position, 30, 30, 66, 2.105263f, 3.684211f, 0, new Color(255, 255, 255), 1f)];
+                    dust.noGravity = true;
+                }
 
+            }
         }
 
         public override void UpdateEquips(ref bool wallSpeedBuff, ref bool tileSpeedBuff, ref bool tileRangeBuff)
