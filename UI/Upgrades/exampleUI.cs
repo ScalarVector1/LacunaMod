5e3a2194edfd780b528bb2b9840cf9917474e3d7
NotAScalie <bobfishkins23@yahoo.com>
2018-05-22 21:36:05 -0400

    UI advances

--- UI/Upgrades/exampleUI.cs
+++ UI/Upgrades/exampleUI.cs
@@ -14,11 +14,33 @@ namespace LacunaMod.UI.Upgrades
     {
         public UIElement UpgradePanel;
         public static bool visible = true;
+        //Cloak textures
         static Texture2D CloakMeterLockedTexture = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_2");
         static Texture2D CloakMeterBlackTexture = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_1");
         static Texture2D CloakMeterChargeTexture = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_0");
         static Texture2D CloakMeterFullTexture = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_3");
-        UIImage CloakMeter = new UIImage(CloakMeterChargeTexture);
+        static Texture2D CloakMeter10 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_10");
+        static Texture2D CloakMeter20 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_20");
+        static Texture2D CloakMeter30 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_30");
+        static Texture2D CloakMeter40 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_40");
+        static Texture2D CloakMeter50 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_50");
+        static Texture2D CloakMeter60 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_60");
+        static Texture2D CloakMeter70 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_70");
+        static Texture2D CloakMeter80 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_80");
+        static Texture2D CloakMeter90 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Cloak/Cloak_90");
+        //Bolt textures
+        static Texture2D BoltMeterLocked = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/Locked");
+        static Texture2D BoltMeter0 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/0");
+        static Texture2D BoltMeterFull = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/Charged");
+        static Texture2D BoltMeter20 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/20");
+        static Texture2D BoltMeter40 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/40");
+        static Texture2D BoltMeter60 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/60");
+        static Texture2D BoltMeter80 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/80");
+        static Texture2D BoltMeter100 = ModLoader.GetTexture("LacunaMod/UI/Upgrades/Bolt/100");
+
+
+        UIImage CloakMeter = new UIImage(CloakMeterLockedTexture);
+        UIImage BoltMeter = new UIImage(BoltMeterLocked);
 
         public override void OnInitialize()
         {
@@ -27,7 +49,7 @@ namespace LacunaMod.UI.Upgrades
             UpgradePanel.Left.Set(400f, 0f);
             UpgradePanel.Top.Set(100f, 0f);
             UpgradePanel.Width.Set(34f, 0f);
-            UpgradePanel.Height.Set(100f, 0f);
+            UpgradePanel.Height.Set(200f, 0f);
 
             UpgradePanel.OnMouseDown += new UIElement.MouseEvent(DragStart);
             UpgradePanel.OnMouseUp += new UIElement.MouseEvent(DragEnd);
@@ -44,6 +66,19 @@ namespace LacunaMod.UI.Upgrades
             CloakMeter.Width.Set(30, 0f);
             CloakMeter.Height.Set(44, 0f);
             UpgradePanel.Append(CloakMeter);
+
+            UIImage BoltMeterBack = new UIImage(BoltMeterLocked);
+            BoltMeterBack.Left.Set(4, 0f);
+            BoltMeterBack.Top.Set(54, 0f);
+            BoltMeterBack.Width.Set(24, 0f);
+            BoltMeterBack.Height.Set(46, 0f);
+            UpgradePanel.Append(BoltMeterBack);
+
+            BoltMeter.Left.Set(4, 0f);
+            BoltMeter.Top.Set(54, 0f);
+            BoltMeter.Width.Set(24, 0f);
+            BoltMeter.Height.Set(46, 0f);
+            UpgradePanel.Append(BoltMeter);
 
             base.Append(UpgradePanel);
         }
@@ -72,10 +107,80 @@ namespace LacunaMod.UI.Upgrades
         {
             Vector2 MousePosition = new Vector2((float)Main.mouseX, (float)Main.mouseY);
 
-            if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer == 0 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)
+            if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer == 0 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true) // if charged and unlocked
             {
                 CloakMeter.SetImage(CloakMeterFullTexture);
             }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 270 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 240 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//4.5 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter10);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 240 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 210 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//4 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter20);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 210 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 180 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//3.5 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter30);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 180 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 150 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//3 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter40);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 150 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 120 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//2.5 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter50);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 120 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 90 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//2 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter60);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 90 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 60 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//1.5 seconds left
+            {
+                CloakMeter.SetImage(CloakMeter70);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 60 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 30 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//1 second left
+            {
+                CloakMeter.SetImage(CloakMeter80);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer <= 30 && Main.LocalPlayer.GetModPlayer<LCPlr>().teletimer >= 0 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)//.5 second left
+            {
+                CloakMeter.SetImage(CloakMeter90);
+            }
+            else if (Main.LocalPlayer.GetModPlayer <LCPlr>().teletimer != 0 && Main.LocalPlayer.GetModPlayer<LCPlr>().TeleCloak == true)// if not charged and unlocked
+            {
+                CloakMeter.SetImage(CloakMeterBlackTexture);
+            }
+            else if (Main.LocalPlayer.GetModPlayer <LCPlr>().TeleCloak == false)// if locked
+            {
+                CloakMeter.SetImage(CloakMeterLockedTexture);
+            }
+            //Bolt
+            if (Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer == 0 && Main.LocalPlayer.GetModPlayer<LCPlr>().Bolt == true) // if charged and unlocked
+            {
+                BoltMeter.SetImage(BoltMeterFull);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer <= 150 && Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer >= 120 && Main.LocalPlayer.GetModPlayer<LCPlr>().Bolt == true)//2.5 seconds left
+            {
+                BoltMeter.SetImage(BoltMeter20);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer <= 120 && Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer >= 90 && Main.LocalPlayer.GetModPlayer<LCPlr>().Bolt == true)//2 seconds left
+            {
+                BoltMeter.SetImage(BoltMeter40);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer <= 90 && Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer >= 60 && Main.LocalPlayer.GetModPlayer<LCPlr>().Bolt == true)//1.5 seconds left
+            {
+                BoltMeter.SetImage(BoltMeter60);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer <= 60 && Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer >= 30 && Main.LocalPlayer.GetModPlayer<LCPlr>().Bolt == true)//1 seconds left
+            {
+                BoltMeter.SetImage(BoltMeter80);
+            }
+            else if (Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer <= 30 && Main.LocalPlayer.GetModPlayer<LCPlr>().bolttimer >= 0 && Main.LocalPlayer.GetModPlayer<LCPlr>().Bolt == true)//2.5 seconds left
+            {
+                BoltMeter.SetImage(BoltMeter100);
+            }
+
             if (UpgradePanel.ContainsPoint(MousePosition))
             {
                 Main.LocalPlayer.mouseInterface = true;
